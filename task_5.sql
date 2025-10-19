0b6e6a0b027e51f0ee7315bc13075e486f391c2f
Favour Japheth <favourjaphet27@gmail.com>
2025-10-19 00:43:40 -0700

    made task_5.sql alx compliant

--- task_5.sql
+++ task_5.sql
@@ -1,10 +1,4 @@
 USE alx_book_store;
 
--- Reset AUTO_INCREMENT so next customer_id starts at 1
-ALTER TABLE Customers AUTO_INCREMENT = 1;
-
--- Remove any existing row with the same email
-DELETE FROM Customers WHERE email = 'cbaidoo@sandtech.com';
-
-INSERT INTO Customers (customer_name, email, address)
-VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
