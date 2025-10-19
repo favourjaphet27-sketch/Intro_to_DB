be269ab1fc02645b5a2e49409b3a3d28f59a45b6
Favour Japheth <favourjaphet27@gmail.com>
2025-10-19 00:38:16 -0700

    Reset auto increment

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,10 @@
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- Reset AUTO_INCREMENT so next customer_id starts at 1
+ALTER TABLE Customers AUTO_INCREMENT = 1;
+
+-- Remove any existing row with the same email
+DELETE FROM Customers WHERE email = 'cbaidoo@sandtech.com';
+
+INSERT INTO Customers (customer_name, email, address)
+VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
